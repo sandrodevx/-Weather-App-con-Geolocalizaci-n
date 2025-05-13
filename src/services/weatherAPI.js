6b213ad8e21cf9aac3d6f963142d51d6d3fb0cf8
Sandro Gomez <sandrodevx@gmail.com>
2025-05-13 21:04:50 +0200

    Update weatherAPI.js

--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = import.meta.env.VITE_APP_WEATHER_API_KEY;
+const API_KEY = process.env.VITE_APP_WEATHER_API_KEY;
 const WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 const FORECAST_BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast';
 
